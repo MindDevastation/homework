f58bb0f9e2d28a026d212b5f959df41eccc52af1
MindDevastation <mindflux99@gmail.com>
2023-09-16 12:40:54 +0300

    homework 5

---
+++ homework/homework_5.h
@@ -0,0 +1,15 @@
+#pragma once
+void h5_get();
+void h5_task1();
+void h5_task2();
+void h5_task3();
+void h5_task4();
+void h5_task5();
+void h5_task6();
+void h5_task7();
+void h5_task8();
+void h5_task9();
+void h5_task10();
+
+double printnum(double button_number, char sign, double result);
+void printsign(char sign);
